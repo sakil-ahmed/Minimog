c380d70f008ed21fcff074820e0d15c19b30fbba
sakil-ahmed <sakilahmed1495@gmail.com>
2022-08-28 09:53:17 +0600

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -80,7 +80,6 @@ export const Header = () => {
     mobile.addEventListener("", mobileDevice);
   }, [
     isActiveLogin,
-    isActive,
     isActiveCart,
     isActiveSearch,
     isActiveRegister,
